efb35008c7a8cda6d23243e8c1c49c5dbee4ab2b
BenCichos <b.cichos@me.com>
2024-05-25 14:09:33 +0200

    fixed issue with runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,14 +16,14 @@ using Test
 
     @testset "Generator" begin
         gnp = GNP(100, 0.5)
-        @test typeof(gnp) === GNP{100}
         graph = gnp()
         graphs = gnp(5)
+
+        @test typeof(gnp) === GNP{100}
         @test typeof(graph) === Graph{Float64, 100}
         @test typeof(graphs) === Vector{Graph{Float64, 100}}
         @test order(graph) == 100
-        @test size(graph) == (100, 100)
         @test !isempty(edges(graph))
         @test ~all(iszero, degrees(graph))
-    end 
+    end
 end;
